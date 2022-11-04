e3a9dd7a323010378f73c6758ba75ea20fd58ece
Noronicky <n0ronicky@gmail.com>
2022-11-04 14:25:23 +0900

    “testCommit”

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use  App\Models\Post;
+use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
-
- {
-     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+{
+    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
   {
-     return $post->get();//$postの中身を戻り値にする
-   }
-
+   return view('welcome'); //$postの中身を戻り値にする。
+  }
 }
+  
